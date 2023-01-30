68fbd5df0d070ba53642b71799effea4d690db97
erick <erisso.333@hotmail.com>
2023-01-29 23:07:33 -0600

    Modal

--- resources/js/services/Product.js
+++ resources/js/services/Product.js
@@ -1,7 +1,7 @@
 export class ProductService {
 
     getProducts = async() => {
-        const response = await fetch('/sigi_barra_medica/public/api/products');
+        const response = await fetch('/api/products');
         const data = await response.json();
         return data;
     }
